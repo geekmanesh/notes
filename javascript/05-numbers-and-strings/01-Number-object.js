1dcf1faaa76984428eda978bb9a06aa28b855d1e
Hamid <geekmanesh@gmail.com>
2026-08-05 14:16:05 +0330

    feat(javascript): add numbers and string notes

---
+++ javascript/05-numbers-and-strings/01-Number-object.js
@@ -0,0 +1,11 @@
+const biggestNum = Number.MAX_VALUE;
+const smallestNum = Number.MIN_VALUE;
+const infiniteNum = Number.POSITIVE_INFINITY;
+const negInfiniteNum = Number.NEGATIVE_INFINITY;
+const notANum = Number.NaN;
+
+console.log("Biggest Number:", biggestNum);
+console.log("Smallest Number:", smallestNum);
+console.log("Positive Infinity:", infiniteNum);
+console.log("Negative Infinity:", negInfiniteNum);
+console.log("Not a Number (NaN):", notANum);
